ea679a7360c2417b667d4f056beb32238bf555fc
xuatz <xzlow10@gmail.com>
2021-12-20 23:17:51 +0900

    feat: tweaks to merge-api + add private registry

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 var express = require('express');
 var app = express();
+var { api1 } = require('@r-tech-org/merge-apis-meta')
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.listen(3000, () => {
+var port = 3000
+app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
+
+
+api1.getUserProfiles(
+  (profiles) => {
+    console.log(profiles)
+  }
+)
