52a906bbd489d481f7d1acce2f96a75135c0c200
xuatz <xzlow10@gmail.com>
2021-12-20 23:43:57 +0900

    feat: tweaks to merge-api

--- src/index.js
+++ src/index.js
@@ -1,19 +1,18 @@
-var express = require('express');
-var app = express();
-var { api1 } = require('@r-tech-org/merge-apis-meta')
+var express = require('express')
+var app = express()
+var { getUserProfiles } = require('@r-tech-org/merge-apis-meta')
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.get('/userProfiles', (req, res) => {
+  getUserProfiles((profiles) => {
+    res.send(profiles)
+  })
 })
 
 var port = 3000
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
-
-
-api1.getUserProfiles(
-  (profiles) => {
-    console.log(profiles)
-  }
-)
